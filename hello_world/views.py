3e5985fe16a718304a473729c2d891efbab8ecb8
unknown <ajean.munro@gmail.com>
2020-03-07 16:51:19 -0800

    move kanye db

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django import forms
 from .forms import LyricInputForm
 from datamuse import datamuse
+import pandas as pd
 
 def index(request): 
     output = ""
@@ -17,10 +18,16 @@ def index(request):
 
 
 def get_output(input_lyric):
+
+    # Get words that rhyme with input word
     api = datamuse.Datamuse()
     output = api.words(rel_rhy=input_lyric, max=50)
     sorted_output = sorted(output, key = lambda i: i['numSyllables'])
     final_output = [x['word'] for x in sorted_output]
+
+    # Import main lyric table
+    lyrics_tbl = pd.read_csv("kanye_lyrics.csv")
+    
 
 
     return final_output
